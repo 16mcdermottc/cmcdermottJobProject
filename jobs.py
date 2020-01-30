e2a17979e074232a973087fa9b1fefc299a47998
16mcdermottc <16mcdermottc@gmail.com>
2020-01-30 14:56:01 -0500

    ghyny

---
+++ jobs.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+def get_api():
+    job_responses = []
+    length = 50
+    i = 1
+
+    while length != 0:
+        response = requests.get("https://jobs.github.com/positions.json?page=" + str(i))
+        i += 1
+
+        for each in response.json():
+            job_responses.append(json.dumps(each, sort_keys=True, indent=4))
+
+        length = len(response.json())
+
+    file = open("jobList.txt", "w+")
+
+    for each in job_responses:
+        file.write(each)
+
+    return job_responses
---
+++ tests/test_jobs.py
@@ -0,0 +1,18 @@
+import pytest
+import jobs
+import os.path
+
+
+def test_api_length():
+    joblist = jobs.get_api()
+    assert len(joblist) > 100
+
+
+def test_job_listing():
+    flag = False
+    file = open(os.path.dirname(__file__) + '/../jobList.txt', "r")
+    for line in file:
+        if 'SkillValue' in line:
+            flag = True
+    assert flag
+    file.close()
