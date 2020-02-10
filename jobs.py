4ccb184ac5173984b19f8921769b3c3e4ae86edd
16mcdermottc <16mcdermottc@gmail.com>
2020-02-10 15:43:02 -0500

    Sprint 2 push pre-lint

--- jobs.py
+++ jobs.py
@@ -1,5 +1,53 @@
 import requests
 import json
+import sqlite3
+from typing import Tuple
+
+
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)
+    cursor = db_connection.cursor()
+    return db_connection, cursor
+
+
+def close_db(connection: sqlite3.Connection):
+    connection.commit()
+    connection.close()
+
+
+def create_table(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (
+                   company TEXT NOT NULL,
+                   company_logo TEXT,
+                   company_url TEXT,
+                   created_at TEXT NOT NULL,
+                   description TEXT,
+                   how_to_apply TEXT,
+                   id TEXT PRIMARY KEY,
+                   location TEXT,
+                   title TEXT,
+                   type TEXT,
+                   url TEXT NOT NULL
+                   );''')
+
+
+def insert_jobs(cursor: sqlite3.Cursor, job_responses):
+    print(len(job_responses))
+    for job in job_responses:
+        cursor.execute(f'''INSERT OR REPLACE INTO jobs (company, company_logo, company_url, 
+                        created_at, description, how_to_apply, id, location, title, type, url) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                       (job["company"],
+                        job["company_logo"],
+                        job["company_url"],
+                        job["created_at"],
+                        job["description"],
+                        job["how_to_apply"],
+                        job["id"],
+                        job["location"],
+                        job["title"],
+                        job["type"],
+                        job["url"]))
 
 
 def get_api():
@@ -16,13 +64,20 @@ def get_api():
             i += 1
 
             for each in response.json():
-                job_responses.append(json.dumps(each, sort_keys=True, indent=4))
+                job_responses.append(each)
 
             length = len(response.json())
 
-    file = open("jobList.txt", "w+")
-
-    for each in job_responses:
-        file.write(each)
-
     return job_responses
+
+
+def main():
+    conn, cursor = open_db("Git_Jobs.sqlite")
+    print(type(conn))
+    create_table(cursor)
+    insert_jobs(cursor, get_api())
+    close_db(conn)
+
+
+if __name__ == '__main__':
+    main()
